6699258d1af453cb5664d85cf01759e07c6f75c2
paulleerentveld <paul@veldnet.com.au>
2022-06-28 17:52:45 +1000

    lab done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :anagram
+
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match(words)
+
+        words.map do |word|
+            word if word.split("").sort == @anagram.split("").sort
+            #puts word.split("").sort.inspect
+        end.compact
+    end
+
+end
+
+
+
+# listen = Anagram.new("listen")
+# puts listen.match(%w(enlists google inlets banana))
